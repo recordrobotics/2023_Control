5a8fee0d848337cae19a8e14a2e1547bdf4d8af6
Rit Garg <rit.g.garg@gmail.com>
2023-01-10 20:44:35 -0500

    Revert "adding programmer_status constant"
    This reverts commit 53cc6371f29d29106c599b6f4d6bf9f8386dc4e5.

--- src/main/java/org/recordrobotics/charger/Constants.java
+++ src/main/java/org/recordrobotics/charger/Constants.java
@@ -12,6 +12,4 @@ package org.recordrobotics.charger;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-    public static final String PROGRAMMER_STATUS = "hype";
-}
+public final class Constants {}
