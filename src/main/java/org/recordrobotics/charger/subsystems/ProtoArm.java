ffa94e971aebc26c8bf3db44b2cd43d9b27e21c4
Rit Garg <rit.g.garg@gmail.com>
2023-01-26 19:57:26 -0500

    ran spotless

--- src/main/java/org/recordrobotics/charger/subsystems/ProtoArm.java
+++ src/main/java/org/recordrobotics/charger/subsystems/ProtoArm.java
@@ -8,15 +8,15 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 public class ProtoArm extends Arm {
 	private CANSparkMax _originMotor = new CANSparkMax(RobotMap.Arm.ORIGIN_MOTOR_PORT, MotorType.kBrushless);
 	private CANSparkMax _changeMotor = new CANSparkMax(RobotMap.Arm.CHANGE_MOTOR_PORT, MotorType.kBrushless);
-    
-    private static final double GEAR_RATIO = 0;
+
+	private static final double GEAR_RATIO = 0;
 
 	public ProtoArm() {
-        _originMotor.set(0);
-        _changeMotor.set(0);
+		_originMotor.set(0);
+		_changeMotor.set(0);
 
-        _originMotor.getEncoder().setPositionConversionFactor(GEAR_RATIO);
-        _changeMotor.getEncoder().setPositionConversionFactor(GEAR_RATIO);
+		_originMotor.getEncoder().setPositionConversionFactor(GEAR_RATIO);
+		_changeMotor.getEncoder().setPositionConversionFactor(GEAR_RATIO);
 	}
 
 	public void moveAngles(double angle1, double angle2) {
