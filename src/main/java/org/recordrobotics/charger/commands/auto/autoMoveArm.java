cef2ebe9ca6ac5153a4dbaf5e2ab64523beffe8f
yuanhengm17 <yuanhengm@gmail.com>
2023-02-16 19:49:28 -0500

    improved the code

--- src/main/java/org/recordrobotics/charger/commands/auto/autoMoveArm.java
+++ src/main/java/org/recordrobotics/charger/commands/auto/autoMoveArm.java
@@ -4,35 +4,37 @@ import org.recordrobotics.charger.subsystems.Arm;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class autoMoveArm extends CommandBase{
-    
-    private Arm _arm;
-    private double _speed;
-    private double _targetX;
-    private double _targetY;
+public class AutoMoveArm extends CommandBase{
 
-    public autoMoveArm(Arm arm, double speed, double targetX, double targetY){
-        if (speed <= 0) {
+	private Arm _arm;
+	private double _speed;
+	private double _targetX;
+	private double _targetY;
+	private double[] _angles;
+
+	public AutoMoveArm(Arm arm, double speed, double targetX, double targetY){
+		if (speed <= 0) {
 			throw new IllegalArgumentException("Speed must be positive");
 		}
 		if (arm == null) {
 			throw new IllegalArgumentException("Arm is null");
 		}
 
-        _arm = arm;
+		_arm = arm;
 		_targetX = targetX;
-        _targetY = targetY;
+		_targetY = targetY;
 		_speed = speed;
 		addRequirements(arm);
-    }
-
-    @Override
-	public void initialize() {
-		_arm.moveAngles(_speed, _arm.getAnglesOfRotation(_targetX, _targetY));
 	}
 
-    @Override
-    public boolean isFinished() {
-		return _targetX == _arm.getAnglesOfRotation(_targetX, _targetY)[0] && _targetY == _arm.getAnglesOfRotation(_targetX, _targetY)[1];
+	@Override
+	public void initialize() {
+		_angles = _arm.getAnglesOfRotation(_targetX, _targetY);
+		_arm.moveAngles(_speed, _angles);
+	}
+
+	@Override
+	public boolean isFinished() {
+		return _targetX == _angles[0] && _targetY == _angles[1];
 	}
 }
