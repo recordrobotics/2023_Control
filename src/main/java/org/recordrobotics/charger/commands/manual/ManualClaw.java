f6b39a09eb44846b0961c66b9bbd403aa72e0562
Rit Garg <rit.g.garg@gmail.com>
2023-02-20 15:58:11 -0500

    possibly fixed error with neutral being closed

--- src/main/java/org/recordrobotics/charger/commands/manual/ManualClaw.java
+++ src/main/java/org/recordrobotics/charger/commands/manual/ManualClaw.java
@@ -9,6 +9,10 @@ public class ManualClaw extends CommandBase {
 	private Claw _claw;
 	private IControlInput _controls;
 
+	private static final double CLAW_NEUTRAL = 0.3;
+	private static final double CLAW_CUBE = 0.1;
+	private static final double CLAW_CONE = 0.0;
+
 	private static final double TURN_SPEED = 0.3;
 	// TODO: find a good value experimentally
 	private static final double ERROR_MARGIN = 0.01;
@@ -32,13 +36,13 @@ public class ManualClaw extends CommandBase {
 		double target;
 		switch (_controls.getClawTurn()) {
 			case CUBE:
-				target = Claw.CLAW_CUBE;
+				target = CLAW_CUBE;
 				break;
 			case CONE:
-				target = Claw.CLAW_CONE;
+				target = CLAW_CONE;
 				break;
 			default:
-				target = Claw.CLAW_NEUTRAL;
+				target = CLAW_NEUTRAL;
 				break;
 		}
 
--- src/main/java/org/recordrobotics/charger/subsystems/Claw.java
+++ src/main/java/org/recordrobotics/charger/subsystems/Claw.java
@@ -11,11 +11,6 @@ public class Claw extends SubsystemBase {
 
 	private static final double GEAR_RATIO = 63;
 
-	// Claw target positions
-	public static final double CLAW_NEUTRAL = 0.0;
-	public static final double CLAW_CUBE = 0.2;
-	public static final double CLAW_CONE = 0.3;
-
 	private CANSparkMax _motor = new CANSparkMax(RobotMap.Claw.MOTOR_PORT, MotorType.kBrushless);
 
 	public Claw() {
@@ -23,15 +18,25 @@ public class Claw extends SubsystemBase {
 		_motor.getEncoder().setPositionConversionFactor(1 / GEAR_RATIO);
 	}
 
-
+	/**
+	 * turns motor
+	 * @param speed speed of the motor
+	 */
 	public void turn(double speed) {
 		_motor.set(Subsystems.limitSpeed(speed));
 	}
 
+	/**
+	 * gets encoder value of 
+	 * @return
+	 */
 	public double getPosition() {
 		return _motor.getEncoder().getPosition();
 	}
 
+	/**
+	 * resets encoder values
+	 */
 	public void resetEncoders() {
 		_motor.getEncoder().setPosition(0);
 	}
