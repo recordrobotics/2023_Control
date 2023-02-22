06fd725b99fb8c7b6db9649d6607430afa59b850
beryliumOfRecordRobotics <programmingberyllium@gmail.com>
2023-02-22 15:20:20 -0500

    ids

--- src/main/java/org/recordrobotics/charger/RobotMap.java
+++ src/main/java/org/recordrobotics/charger/RobotMap.java
@@ -21,14 +21,14 @@ public class RobotMap {
 	 */
 	public class DriveBase {
 		public static final int LEFT_BACK_MOTOR_PORT = 2;
-		public static final int LEFT_MIDDLE_MOTOR_PORT = 0;
+		public static final int LEFT_MIDDLE_MOTOR_PORT = 3;
 		public static final int LEFT_FRONT_MOTOR_PORT = 1;
-		public static final int RIGHT_BACK_MOTOR_PORT = 4;
-		public static final int RIGHT_MIDDLE_MOTOR_PORT = 0;
-		public static final int RIGHT_FRONT_MOTOR_PORT = 3;
+		public static final int RIGHT_BACK_MOTOR_PORT = 5;
+		public static final int RIGHT_MIDDLE_MOTOR_PORT = 6;
+		public static final int RIGHT_FRONT_MOTOR_PORT = 4;
 	}
 
 	public class Claw {
-		public static final int MOTOR_PORT = 2;
+		public static final int MOTOR_PORT = 7;
 	}
 }
