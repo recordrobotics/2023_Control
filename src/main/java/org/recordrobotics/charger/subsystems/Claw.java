4f084cfa96ecebe7d0fb84fe2d33fdacf88f087a
beryliumOfRecordRobotics <programmingberyllium@gmail.com>
2023-02-23 16:09:37 -0500

    claw voltage testing

--- src/main/java/org/recordrobotics/charger/subsystems/Claw.java
+++ src/main/java/org/recordrobotics/charger/subsystems/Claw.java
@@ -1,7 +1,11 @@
 package org.recordrobotics.charger.subsystems;
 
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import org.recordrobotics.charger.Constants;
 import org.recordrobotics.charger.RobotMap;
 
 import com.revrobotics.CANSparkMax;
@@ -13,9 +17,19 @@ public class Claw extends SubsystemBase {
 
 	private CANSparkMax _motor = new CANSparkMax(RobotMap.Claw.MOTOR_PORT, MotorType.kBrushless);
 
+	private GenericEntry _encoderEntry;
+
 	public Claw() {
 		_motor.set(0);
 		_motor.getEncoder().setPositionConversionFactor(1 / GEAR_RATIO);
+
+		ShuffleboardTab tab = Shuffleboard.getTab(Constants.constants.DATA_TAB);
+		_encoderEntry = tab.add("Drive Encoders", 0).getEntry();
+	}
+
+	@Override
+	public void periodic() {
+		_encoderEntry.setDouble(_motor.getBusVoltage());
 	}
 
 	/**
