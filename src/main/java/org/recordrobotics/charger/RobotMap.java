b7a51833a4a65263120bd1d815541f7f3a27cf73
scaryjoshie <scaryjoshie@gmail.com>
2023-03-07 20:30:25 -0500

    Update RobotMap.java

--- src/main/java/org/recordrobotics/charger/RobotMap.java
+++ src/main/java/org/recordrobotics/charger/RobotMap.java
@@ -8,12 +8,17 @@ public class RobotMap {
 	 * Arm Ports (Phoenix) - will change once decided
 	 * Ports will also be in SIM/Neo - talk to Jake for more info
 	 */
+
+	/*
+	
 	public class Arm {
 		/** the motor that will be (0,0) in the calculation */
-		public static final int ORIGIN_MOTOR_PORT = 8;
+		//public static final int ORIGIN_MOTOR_PORT = 8;
 		/** the motor that will change coords in the calculation */
-		public static final int CHANGE_MOTOR_PORT = 7;
-	}
+		//public static final int CHANGE_MOTOR_PORT = 7;
+	//}
+
+	
 
 	/**
 	 * Control ports (PC USB)
@@ -39,9 +44,11 @@ public class RobotMap {
 		public static final int RIGHT_FRONT_MOTOR_PORT = 4;
 	}
 
+	/*
 	public class Claw {
 		public static final int LIMIT_SWITCH = 9;//the pwm port is port 9
 
 		public static final int MOTOR_PORT = 2;//was 9
 	}
+	*/
 }
