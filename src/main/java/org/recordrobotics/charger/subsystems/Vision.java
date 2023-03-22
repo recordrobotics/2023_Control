c1eb355366f3e9a233ea2f259040112261704ae1
scaryjoshie <scaryjoshie@gmail.com>
2023-03-22 15:56:20 -0400

    updated vision code

--- src/main/java/org/recordrobotics/charger/subsystems/Vision.java
+++ src/main/java/org/recordrobotics/charger/subsystems/Vision.java
@@ -1,5 +1,6 @@
 package org.recordrobotics.charger.subsystems;
 
+//import org.apache.commons.collections4.functors.TransformerClosure;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -8,13 +9,16 @@ import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 @SuppressWarnings({"PMD.SystemPrintln", "PMD.FieldNamingConventions"})
 public class Vision extends SubsystemBase{
+
 
 	public PhotonCamera camera = new PhotonCamera("OV5647"); //IMPORTANT: This camera name MUST match the one on the Raspberry Pi, accessible through the PhotonVision UI.
 	public Transform3d robotToCam = new Transform3d(new Translation3d(Units.inchesToMeters(11), -1*Units.inchesToMeters(9), 0.1725), new Rotation3d(0,0,0)); //The offset from the center of the robot to the camera, and from facing exactly forward to the orientation of the camera.
 	//TODO: SET TRANSFORM!!!!!!
-
 
 	// Sends photonvision camera output to shuffleboard
 	//public void runDriverMode() {
@@ -22,56 +26,62 @@ public class Vision extends SubsystemBase{
 	//	camera.setPipelineIndex(2);
 	//}
 
-	static final Transform2d[] tag_transforms = {//april tags 1-8 in order. values contained are x, y, z, theta, in that order. x, y, z are distances in meters, theta is in radians.
+	static Transform3d[] tags = {//april tags 1-8 in order. values contained are x, y, z, theta, in that order. x, y, z are distances in meters, theta is in radians.
 		
-		new Transform2d(new Translation2d(15.513558, 4.424426), new Rotation2d(Math.PI)), //tag 1
-		new Transform2d(new Translation2d(15.513558, 2.748026), new Rotation2d(Math.PI)), //tag 2
-		new Transform2d(new Translation2d(15.513558, 4.424426), new Rotation2d(Math.PI)), //tag 3
-		new Transform2d(new Translation2d(16.178784, 6.749796), new Rotation2d(Math.PI)), //tag 4
-		new Transform2d(new Translation2d(0.36195, 6.749796), new Rotation2d(0)), //tag 5
-		new Transform2d(new Translation2d(1.02743, 4.424426), new Rotation2d(0)), //tag 6
-		new Transform2d(new Translation2d(1.02743, 2.748026), new Rotation2d(0)), //tag 7
-		new Transform2d(new Translation2d(1.02743, 1.071626), new Rotation2d(0)),}; //tag 8
+		new Transform3d(new Translation3d(15.513558, 1.071626, 0.462788), new Rotation3d(0,0,Math.PI)),
+		new Transform3d(new Translation3d(15.513558, 2.748026, 0.462788), new Rotation3d(0,0,Math.PI)),
+		new Transform3d(new Translation3d(15.513558, 4.424426, 0.462788), new Rotation3d(0,0,Math.PI)),
+		new Transform3d(new Translation3d(16.178784, 6.749796, 0.695452), new Rotation3d(0,0,Math.PI)),
+		new Transform3d(new Translation3d(0.36195, 6.749796, 0.695452), new Rotation3d(0,0,0)),
+		new Transform3d(new Translation3d(1.02743, 4.424426, 0.462788), new Rotation3d(0,0,0)),
+		new Transform3d(new Translation3d(1.02743, 2.748026, 0.462788), new Rotation3d(0,0,0)),
+		new Transform3d(new Translation3d(1.02743, 1.071626, 0.462788), new Rotation3d(0,0,0))
+	};
 	
 	public static double[] estimateGlobalPose(PhotonCamera camera) {
+		System.out.println("vision detected!");
 
 		// Gets a frame from the camera
 		var result = camera.getLatestResult();
 
-		// Checks if there is a target. If there is one, the "if" statement passes. 
-		boolean hasTargets = result.hasTargets();
-		if (hasTargets){
-			
 			// Gets target object from apriltag perspective photonvision
 			PhotonTrackedTarget target = result.getBestTarget();
+			// Gets the Transform3d object for april to robot
 			Transform3d robot_to_april = target.getBestCameraToTarget()/*.plus(robotToCam.inverse())*/; // you could put the offset here if you were testing for reals
 			Transform3d april_to_robot = robot_to_april.inverse();
-			// Converts the Transform3d object into a Pose2d object. Probably not entirely necessary to shift everything to 2d but its 2:30 in the morning and honestly my brain is kind of not working
-			Pose2d april_to_robot_pose2d = new Pose2d(
-				april_to_robot.getTranslation().toTranslation2d(), 
-				april_to_robot.getRotation().toRotation2d());
-
-			// Gets the fiducial ID and uses it to get the correct transform 2d object, which it then inverses to get the april to global perspective
+			// Gets the fiducial ID and uses it to get the correct transform 2d object
 			int targetID = target.getFiducialId();
-			Transform2d april_to_global = tag_transforms[targetID - 1].inverse();
+			Transform3d global_to_april = tags[targetID - 1];
+			// Adds the two transform objects together to get robot to global
+			Transform3d robot_to_global = global_to_april.plus(april_to_robot);
 
-			//System.out.println(april_to_global);
 			
-			// Uses the "Transform2d(Pose2d initial, Pose2d final)" feature to take the difference between april to robot and april to global
-			Pose2d global_to_camera = april_to_robot_pose2d.plus(april_to_global);
+			// Calculates pitch, roll, and yaw
+			double w = robot_to_april.getRotation().getQuaternion().getW();
+			double x = robot_to_april.getRotation().getQuaternion().getX();
+			double y = robot_to_april.getRotation().getQuaternion().getY();
+			double z = robot_to_april.getRotation().getQuaternion().getZ();
+			
+			double roll  = Math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z);
+			double pitch = Math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z);
+			double yaw   = Math.asin(2*x*y + 2*z*w);
 
-			// Gets the X and Y or the transform
-			double global_x = global_to_camera.getX();
-			double global_y = global_to_camera.getY();
-			double global_theta = global_to_camera.getRotation().getRadians();
+
+			//Puts the important stuff on the SmartDashboard
+			//SmartDashboard.putNumber("X value", robot_to_global.getX());
+			//SmartDashboard.putNumber("Y value", robot_to_global.getY());
+			//SmartDashboard.putNumber("Z value", robot_to_global.getZ());
+			//SmartDashboard.putNumber("Angle (degrees)", robot_to_global.getRotation().toRotation2d().getDegrees());
+
 
 			// Returns final global pose
-			//System.out.println("POSE (x, y, radians): (" + global_x + ", " + global_y + ", " + global_theta + ")");
-			return new double[] {global_x, global_y, global_theta}; // If this line doesnt work create a double[] called "global_pose" and return that instead
+			return new double[] {
+				robot_to_global.getX(), 
+				robot_to_global.getY(), 
+				robot_to_global.getZ(),
+				pitch, roll, yaw}; 
 		}
 
-		else {return null;}
-	}
 
 	public static boolean checkForTarget(PhotonCamera camera){
 		var result = camera.getLatestResult();//get a frame from the camera
