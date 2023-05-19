012ed7c806f49dd5089432808993fa232432ed26
Blaukreutz <100523449+Blaukreutz@users.noreply.github.com>
2023-05-19 18:06:36 -0400

    Turnin' PID up to 11

--- src/main/java/org/recordrobotics/Mitocondrion/commands/auto/ChargeStationBalance.java
+++ src/main/java/org/recordrobotics/Mitocondrion/commands/auto/ChargeStationBalance.java
@@ -4,14 +4,21 @@ import org.recordrobotics.Mitocondrion.subsystems.Drive;
 import org.recordrobotics.Mitocondrion.subsystems.NavSensor;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ChargeStationBalance extends CommandBase {
 	private PIDController _pid;
 	private Drive _drive;
 	private NavSensor _nav;
+	//private SimpleMotorFeedforward _feedForward;
+	private double encoderAtZero;
+	
+	private double _PIDTolerance = 0.005;
+	private double _angleTolerance = 0.0524;
 
-	private double _tolerance = 0.0524;
 
 	/**
 	 * sets up PID controller with p,i, and d values
@@ -21,10 +28,14 @@ public class ChargeStationBalance extends CommandBase {
 	 * @param nav navigation sensor object, used here as a vertical gyro
 	 */
 	public ChargeStationBalance(Drive drive, NavSensor nav){
-		_pid = new PIDController(2, 0, 0);
+		_pid = new PIDController(11, 0, 0);
+		//_feedForward = new SimpleMotorFeedforward(0, 0);
 		_drive = drive;
 		_nav = nav;
-		_pid.setTolerance(_tolerance);
+		_pid.setTolerance(_PIDTolerance);
+		if (-1*_angleTolerance <= _nav.getPitch() && _nav.getPitch() <= _angleTolerance){
+			encoderAtZero = (_drive.getLeftEncoder() + _drive.getRightEncoder())/2;
+		}
 
 	}
 
@@ -33,7 +44,8 @@ public class ChargeStationBalance extends CommandBase {
 	 */
 	@Override
 	public void execute() {
-		double _speed = _pid.calculate(_nav.getPitch(), 0);
+		
+		double _speed = _pid.calculate((_drive.getLeftEncoder() + _drive.getRightEncoder())/2, encoderAtZero);
 
 		// Checks for max
 		double SPEED_THRESHOLD = 0.45;
@@ -43,10 +55,14 @@ public class ChargeStationBalance extends CommandBase {
 		}
 		// Prints
 		System.out.println("Speed: " + _speed);
+		SmartDashboard.putNumber("Speed", _speed);
+		SmartDashboard.putNumber("Pitch", _nav.getPitch());
+		SmartDashboard.putNumber("position", (_drive.getLeftEncoder() + _drive.getRightEncoder())/2);
+		SmartDashboard.putNumber("0 position", encoderAtZero);
 		//System.out.println("pitch " + _nav.getPitch());
 
 		// Moves
-		_drive.move(-1*_speed, 0);
+		_drive.move(_speed, 0);//was negative
 		}
 
 	/**
