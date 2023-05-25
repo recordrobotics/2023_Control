c16cc872b6c7d0652763b62a26ac47d1849d80d8
Ethan Djajadi <ethan.djajadi@gmail.com>
2023-05-25 13:26:14 -0400

    name fixing

--- src/main/java/org/recordrobotics/Mitochondrion/commands/auto/SimpleScoreAndTaxi.java
+++ src/main/java/org/recordrobotics/Mitochondrion/commands/auto/SimpleScoreAndTaxi.java
@@ -1,9 +1,8 @@
-package org.recordrobotics.Mitocondrion.commands.auto;
+package org.recordrobotics.Mitochondrion.commands.auto;
 
-import org.recordrobotics.Mitocondrion.commands.manual.ArmPosition;
-import org.recordrobotics.Mitocondrion.subsystems.Arm;
-//import org.recordrobotics.Mitocondrion.subsystems.Claw;
-import org.recordrobotics.Mitocondrion.subsystems.Drive;
+import org.recordrobotics.Mitochondrion.commands.manual.ArmPosition;
+import org.recordrobotics.Mitochondrion.subsystems.Arm;
+import org.recordrobotics.Mitochondrion.subsystems.Drive;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
